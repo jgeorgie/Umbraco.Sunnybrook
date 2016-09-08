20a93fd3ea3955010a03a67092cfc71b3f5f8f02
ashnalbandyan <ashnalbandyan@gmail.com>
2016-09-08 20:29:43 +0400

    fexes

--- Sunnybrook.Umbraco/SunnybrookMontessori/App_Data/Models/all.generated.cs
+++ Sunnybrook.Umbraco/SunnybrookMontessori/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "35b7b10cce2789ce")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
