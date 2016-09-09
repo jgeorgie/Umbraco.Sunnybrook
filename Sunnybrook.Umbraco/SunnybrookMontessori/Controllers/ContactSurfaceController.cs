9355463a7b24a0e18b9e3b8e8c0ecd19f4e21736
Garik Hakobyan <g.a.hakobyan@gmail.com>
2016-09-09 16:13:27 +0400

    l

---
+++ Sunnybrook.Umbraco/SunnybrookMontessori/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,29 @@
+﻿using SunnybrookMontessori.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace SunnybrookMontessori.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        public ActionResult ShowForm()
+        {
+            ContactModel myModel = new ContactModel();
+            //myModel.Email=CurrentPage.GetProperty
+            return PartialView("ContactForm", myModel);
+        }
+        public ActionResult HandleFormPost(ContactModel model)
+        {
+            var newComment = Services.ContentService.CreateContent(model.Name + " - " + DateTime.Now.ToString("dd-MM-yyyy HH:mm"), CurrentPage.Id, "contactUs");
+            newComment.SetValue("emailForm", model.Email);
+            newComment.SetValue("contactName", model.Name);
+            newComment.SetValue("contactMessage", model.Message);
+            Services.ContentService.SaveAndPublishWithStatus(newComment);
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
